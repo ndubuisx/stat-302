6a60a43bc352f41d58acf101dbf1a56f8f76c232
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2022-01-24 11:40:02 -0500

    first commit

---
+++ jan-19-r-script.R
@@ -0,0 +1,15 @@
+# Author: Emmanuel Ndubuisi; Date: Jan 19 2022; Purpose: Install R packages
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("dplyr")
+if(!require(devtools)) install.packages("devtools") devtools::install_github("kassambara/ggpubr")
+
+# Check the installed libraries
+library(ggpubr)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+PATH <- "https://covidtracking.com/data/download/national-history.csv"
+data <- read.csv(PATH)
