a6cecadfabd0902bf46935029ade8914eff92f22
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2022-01-26 11:58:56 -0500

    Update jan-26-r-script.R

--- jan-26-r-script.R
+++ jan-26-r-script.R
@@ -6,6 +6,28 @@
 PATH <- "https://covidtracking.com/data/download/national-history.csv"
 data <- read.csv(PATH, header=TRUE)
 
-# checks the dimension of data frame
+# check the dimension of data frame
 # [1] 420  17 -> 420 rows and 17 columns
 dim(data)
+
+# check names of colums in data frame
+names(data)
+
+# select two columns from data (date and positive) and store in new data frame named data2
+# data2 <- select(data, c("date", "positive"))
+# data2 <- data %>% select("date", "positive")
+data2 <- data.frame(data$date, data$positive)
+
+# summary of data
+summary(data)
+
+# checking the data structure
+is.data.frame(data)
+is.matrix(data)
+
+# checking the data structure
+is.numeric(data)
+is.numeric(data$death)
+
+# check the missing values
+is.na(data$death)
