9a74646053849802c19f09eef327590b37dd1f3a
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2022-02-02 11:51:41 -0500

    Create feb-2-r-script.R

---
+++ feb-2/feb-2-r-script.R
@@ -0,0 +1,25 @@
+# Author: Emmanuel Ndubuisi; Date: Feb 2 2022; Purpose: Generate different types of plots using the ggplot library
+
+# sets the working directory for script
+# setwd("")
+
+PATH <- "https://covidtracking.com/data/download/national-history.csv"
+data <- read.csv(PATH, header=TRUE)
+
+# remove scientific notation and replot x-scale
+options(scipen=999)
+
+# plotting with ggplot2
+library(ggplot2)
+
+# selecting first data rows from the data and storing it into a new dataframe (data2)
+data2 <- data[1:10,]
+
+# plotting on data2
+p <- ggplot(data2, aes(x=death, y=onVentilatorCurrently)) + geom_bar(stat="identity")
+
+# ommit n/a values
+data = na.omit(data)
+
+# plotting on data without n/a values
+p1 <- ggplot(data, aes(x=death, y=onVentilatorCurrently)) + geom_bar(stat="identity")
